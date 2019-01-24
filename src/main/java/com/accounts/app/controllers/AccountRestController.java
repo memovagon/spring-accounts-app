01b24037797146228c16d17d020167194536b5b1
memovagon <46949924+memovagon@users.noreply.github.com>
2019-01-24 02:11:10 -0600

    Integrate AccountsRestController Class

--- src/main/java/com/accounts/app/controllers/AccountRestController.java
+++ src/main/java/com/accounts/app/controllers/AccountRestController.java
@@ -1,13 +1,9 @@
 package com.accounts.app.controllers;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
